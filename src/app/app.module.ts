ea7e9e7a92e5608931df983906273d2612001435
rickmurdock <rick.murdock@live.com>
2017-12-04 23:31:26 -0600

    Added my hero list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
